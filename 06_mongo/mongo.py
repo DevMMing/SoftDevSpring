5d523e1ceeb62d90c73c2383fa18e835dbcb59a7
DevMMing <mming@stuy.edu>
2019-03-03 18:30:36 -0500

    fix

--- 06_mongo/mongo.py
+++ 06_mongo/mongo.py
@@ -5,9 +5,22 @@
 
 import pymongo 
 
-SERVER_ADDR= "lisa.stuy.edu" #"206.189.227.59"
+SERVER_ADDR= "0.0.0.0" #"206.189.227.59"
 connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.test
 collection = db.restaurants
-print(collection.find({"borough": "Brooklyn"}))
+def findBorough(b):
+    return list(collection.find({"borough": b}))
 
+def findZip(z):
+    return list(collection.find({"address.zip": z}))
+
+def findZipGrade(zip, grade):
+    return list(collection.find({"address.zip": zip, "grades.0.grade": grade}))
+
+def findZipScore(zip, score):
+    return list(collection.find({"address.zip": zip, "grades.0.score": {"$lt": score}}))
+print(findBorough("Manhattan"))
+print(findZip("10462"))
+print(findZipGrade("10462", "A"))
+print(findZipScore("10462", 10))
