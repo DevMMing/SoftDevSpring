d00d07be1e0e1b808f4ffe0afb1074d3cb21b74d
DevMMing <mming@stuy.edu>
2019-03-14 00:10:58 -0400

    done

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -7,7 +7,7 @@
 var pic = document.getElementById("vimage");
 
 //size of dot
-const radius = 5;
+const radius = 25;
 
 //gets button with id "clear" and runs the following function when clicked
 document.getElementById('but_clear').addEventListener('click', function(e)
@@ -24,20 +24,35 @@ function getMousePos(e)
 	return { x: e.offsetX, y: e.offsetY };
 }
 
-var children = [];
-
+function random(x)
+{
+	return Math.floor(Math.random()*x)
+}
 //pic == canvas
 pic.addEventListener('click', function(e)
 	{
+		color=e.target.getAttribute("fill");
 		var mousePos = getMousePos(e);
 		console.log(mousePos);
-		var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-		c.setAttribute("cx", mousePos.x);
-		c.setAttribute("cy", mousePos.y);
-		c.setAttribute("r", radius);
-		c.setAttribute("fill", "red");
-		c.setAttribute("stroke", "black");
-		pic.appendChild(c);
-		children.push(mousePos);
-		console.log(children);
+		//clicking on "canvas"
+		if(color == null){
+			var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+			c.setAttribute("cx", mousePos.x);
+			c.setAttribute("cy", mousePos.y);
+			c.setAttribute("r", radius);
+			c.setAttribute("fill", "red");
+			c.setAttribute("stroke", "black");
+			pic.appendChild(c);
+			a=pic.childNodes
+		}
+		//clicking on red dot
+		else if(color == "red"){
+			e.target.setAttribute("fill","blue");
+		}
+		//clicking on blue dot
+		else{
+			e.target.setAttribute("cx",random(501));
+			e.target.setAttribute("cy",random(501));
+			e.target.setAttribute("fill","red");
+		}
 	});
