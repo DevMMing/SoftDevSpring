2d9e3d9862224931765442d2b0764b080a4ef0e6
DevMMing <mming@stuy.edu>
2019-04-30 01:07:13 -0400

    added more cmnts

--- 21_js-mfr/script.js
+++ 21_js-mfr/script.js
@@ -10,21 +10,26 @@ d3.csv("nyc.csv").then(function(data) {
    var fifth_grade=data.map(function(fifth){
 	   return fifth.grade5;
    });
+   //all students enrolled
    var amount=total_enroll.length;
    console.log(amount);
    //filtered school without fifth graders
    var filt_fifth_grade=fifth_grade.filter(function(ele){
 	   return ele != "";
    });
+   //amount in filtered
    var filt_amount =filt_fifth_grade.length
    console.log(filt_amount);
    //filtered eligible students for percentage
    var filt_eligible=filt_fifth_grade.filter(function(ele){
 	return ele >=50;
    });
+   //amount in eligible
    var elig_amount = filt_eligible.length;
+   
 	var a=document.getElementById("median");
 	a.innerHTML=median(total_enroll);
+	
 	var b = document.getElementById("percent");
 	b.innerHTML=String(100*elig_amount/filt_amount)+"%";
 });
